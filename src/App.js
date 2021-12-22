81fa35cfbcbfc3d097269299e96c96e8f4ec30db
Ignacio Giannini <nachogiannini@gmail.com>
2021-12-22 01:51:53 -0300

    interfaz react (no validacion)

--- src/App.js
+++ src/App.js
@@ -31,6 +31,164 @@ function App() {
     ToggleModal_Delete();
   };
 
+/*-------------------------------------------------------------------------------------------------------*/
+
+const form = document.getElementById('form');
+const form2 = document.getElementById('form2');
+const producttype = document.getElementById('producttype');
+const suppliername = document.getElementById('suppliername');
+const postalcode = document.getElementById('postalcode');
+const address = document.getElementById('address');
+const productname = document.getElementById('productname');
+const material = document.getElementById('material');
+const colour = document.getElementById('colour');
+const producttype2 = document.getElementById('producttype2');
+const suppliername2 = document.getElementById('suppliername2');
+const postalcode2 = document.getElementById('postalcode2');
+const address2 = document.getElementById('address2');
+const productname2 = document.getElementById('productname2');
+const material2 = document.getElementById('material2');
+const colour2 = document.getElementById('colour2');
+
+if(form){
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    checkInputs();
+  });
+}
+
+if(form2){
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    checkInputs();
+  });
+}
+
+
+
+function checkInputs() {
+	const producttypeValue = producttype.value.trim();
+	const suppliernameValue = suppliername.value.trim();
+	const postalcodeValue = postalcode.value.trim();
+	const addressValue = address.value.trim();
+	const productnameValue = productname.value.trim();
+	const materialValue = material.value.trim();
+	const colourValue = colour.value.trim();
+
+  const producttypeValue2 = producttype2.value.trim();
+	const suppliernameValue2 = suppliername2.value.trim();
+	const postalcodeValue2 = postalcode2.value.trim();
+	const addressValue2 = address2.value.trim();
+	const productnameValue2 = productname2.value.trim();
+	const materialValue2 = material2.value.trim();
+	const colourValue2 = colour2.value.trim();
+	
+	if(producttypeValue === '') {
+		setErrorFor(producttype, 'Product type cannot be blank');
+	} else {
+		setSuccessFor(producttype);
+	}
+	
+	if(suppliernameValue === '') {
+		setErrorFor(suppliername, 'Supplier name cannot be blank');
+	} 
+	else 
+	{
+		setSuccessFor(suppliername);
+	}
+	
+	if(postalcodeValue === '') {
+		setErrorFor(postalcode, 'Postal code cannot be blank');
+	} else {
+		setSuccessFor(postalcode);
+	}
+	
+	if(addressValue === '') {
+		setErrorFor(address, 'Address cannot be blank');
+	} else{
+		setSuccessFor(address);
+	}
+
+	if(productnameValue === '') {
+		setErrorFor(productname, 'Product name cannot be blank');
+	} else{
+		setSuccessFor(productname);
+	}
+
+	if(materialValue === '') {
+		setErrorFor(material, 'Material cannot be blank');
+	} else{
+		setSuccessFor(material);
+	}
+
+	if(colourValue === '') {
+		setErrorFor(colour, 'Colour cannot be blank');
+	} else{
+		setSuccessFor(colour);
+	}
+
+
+  if(producttypeValue2 === '') {
+		setErrorFor(producttype2, 'Product type cannot be blank');
+	} else {
+		setSuccessFor(producttype2);
+	}
+	
+	if(suppliernameValue2 === '') {
+		setErrorFor(suppliername2, 'Supplier name cannot be blank');
+	} 
+	else 
+	{
+		setSuccessFor(suppliername2);
+	}
+	
+	if(postalcodeValue2 === '') {
+		setErrorFor(postalcode2, 'Postal code cannot be blank');
+	} else {
+		setSuccessFor(postalcode2);
+	}
+	
+	if(addressValue2 === '') {
+		setErrorFor(address2, 'Address cannot be blank');
+	} else{
+		setSuccessFor(address2);
+	}
+
+	if(productnameValue2 === '') {
+		setErrorFor(productname2, 'Product name cannot be blank');
+	} else{
+		setSuccessFor(productname2);
+	}
+
+	if(materialValue2 === '') {
+		setErrorFor(material2, 'Material cannot be blank');
+	} else{
+		setSuccessFor(material2);
+	}
+
+	if(colourValue2 === '') {
+		setErrorFor(colour2, 'Colour cannot be blank');
+	} else{
+		setSuccessFor(colour2);
+	}
+
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+/*-------------------------------------------------------------------------------------------------------*/
   const createMap = (ProductSelected) => {
     setSelectedProduct({
       idProduct: ProductSelected.idProduct,
@@ -108,7 +266,8 @@ function App() {
   };
 
   const Post = async () => {
-    await axios
+   
+      await axios
       .post(baseURL, createProduct_(selectedProduct))
       .then((response) => {
         ToggleModal();
@@ -117,10 +276,13 @@ function App() {
       .catch((error) => {
         console.log(error);
       });
+    
+      
   };
 
   const Put = async () => {
-    await axios
+    
+      await axios
       .put(
         baseURL + "/" + selectedProduct.idProduct,
         createProduct_(selectedProduct, true)
@@ -132,6 +294,8 @@ function App() {
       .catch((error) => {
         console.log(error);
       });
+    
+       
   };
 
   const Delete = async () => {
@@ -207,11 +371,12 @@ function App() {
         </tbody>
       </table>
 
+      {/* <form id="form" className="row g-3 needs-validation" noValidate>     */}
       <Modal isOpen={modalInsert}>
         <ModalHeader>Insert Products</ModalHeader>
         <ModalBody>
           <div className="form-group">
-            <label for="producttype">Product Type: </label>
+            <label for="producttype" id="producttype">Product Type: </label>
             <br />
             <input
               type="text"
@@ -220,83 +385,109 @@ function App() {
               id="producttype"
               onChange={handleChange}
               value={selectedProduct?.productType_}
+              required
             />
-            <label>Supplier Name: </label>
+            {/* <small>Error message</small> */}
+            <label id="suppliername">Supplier Name: </label>
             <br />
             <input
               type="text"
               className="form-control"
               name="supplierName"
+              id="suppliername"
               onChange={handleChange}
               value={selectedProduct?.supplierName}
+              required
             />
-            <label>Phone: </label>
+            {/* <small>Error message</small> */}
+            <label id="phone">Phone: </label>
             <br />
             <input
               type="text"
               className="form-control"
               name="phone"
+              id="phone"
               onChange={handleChange}
               value={selectedProduct?.phone}
+              required
             />
-            <label>Postal Code: </label>
+            {/* <small>Error message</small> */}
+            <label id="postalcode">Postal Code: </label>
             <br />
             <input
               type="text"
               className="form-control"
               name="postalCode"
+              id="postalcode"
               onChange={handleChange}
               value={selectedProduct?.postalCode}
+              required
             />
-            <label>Address: </label>
+            {/* <small>Error message</small> */}
+            <label id="address">Address: </label>
             <br />
             <input
               type="text"
               className="form-control"
               name="address"
+              id="address"
               onChange={handleChange}
               value={selectedProduct?.address}
+              required
             />
-            <label>Product Name: </label>
+            {/* <small>Error message</small> */}
+            <label id="productname">Product Name: </label>
             <br />
             <input
               type="text"
               className="form-control"
               name="productName"
+              id="productname"
               onChange={handleChange}
               value={selectedProduct?.productName}
+              required
             />
-            <label>Material: </label>
+            {/* <small>Error message</small> */}
+            <label id="material">Material: </label>
             <br />
             <input
               type="text"
               className="form-control"
               name="material"
+              id="material"
               onChange={handleChange}
               value={selectedProduct?.material}
+              required
             />
-            <label>Colour: </label>
+            {/* <small>Error message</small> */}
+            <label id="colour">Colour: </label>
             <br />
             <input
               type="text"
               className="form-control"
               name="colour"
+              id="colour"
               onChange={handleChange}
               value={selectedProduct?.colour}
+              required
             />
-            <label>Quantity: </label>
+            {/* <small>Error message</small> */}
+            <label id="quantity">Quantity: </label>
             <br />
             <input
               type="number"
               min="1"
-              max="999"
+              max="9999"
               step="1"
               className="form-control"
               name="quantity"
+              id="quantity"
               onChange={handleChange}
               value={selectedProduct?.quantity}
+              required
             />
-            <label>Price: </label>
+            {/* <small>Error message</small> */}
+            <label id="price">Price: </label>
             <br />
             <input
               type="number"
@@ -305,8 +496,10 @@ function App() {
               step="1"
               className="form-control"
               name="price"
+              id="price"
               onChange={handleChange}
               value={selectedProduct?.price}
+              required
             />
           </div>
         </ModalBody>
@@ -320,8 +513,9 @@ function App() {
           </button>
         </ModalFooter>
       </Modal>
-
+    {/* </form> */}
       {
+        // <form id="form2" className="row g-3 needs-validation" noValidate>
         <Modal isOpen={modalEdit}>
           <ModalHeader>Edit Product</ModalHeader>
           <ModalBody>
@@ -332,73 +526,96 @@ function App() {
                 type="text"
                 className="form-control"
                 name="productType_"
-                id="producttype"
+                id="producttype2"
                 onChange={handleChange}
                 value={selectedProduct?.productType_}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Supplier Name: </label>
               <br />
               <input
                 type="text"
                 className="form-control"
                 name="supplierName"
+                id="suppliername2"
                 onChange={handleChange}
                 value={selectedProduct?.supplierName}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Phone: </label>
               <br />
               <input
                 type="text"
                 className="form-control"
                 name="phone"
+                id="phone2"
                 onChange={handleChange}
                 value={selectedProduct?.phone}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Postal Code: </label>
               <br />
               <input
                 type="text"
                 className="form-control"
                 name="postalCode"
+                id="postalcode2"
                 onChange={handleChange}
                 value={selectedProduct?.postalCode}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Address: </label>
               <br />
               <input
                 type="text"
                 className="form-control"
                 name="address"
+                id="address2"
                 onChange={handleChange}
                 value={selectedProduct?.address}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Product Name: </label>
               <br />
               <input
                 type="text"
                 className="form-control"
                 name="productName"
+                id="productname2"
                 onChange={handleChange}
                 value={selectedProduct?.productName}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Material: </label>
               <br />
               <input
                 type="text"
                 className="form-control"
                 name="material"
+                id="material2"
                 onChange={handleChange}
                 value={selectedProduct?.material}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Colour: </label>
               <br />
               <input
                 type="text"
                 className="form-control"
                 name="colour"
+                id="colour2"
                 onChange={handleChange}
                 value={selectedProduct?.colour}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Quantity: </label>
               <br />
               <input
@@ -408,9 +625,12 @@ function App() {
                 step="1"
                 className="form-control"
                 name="quantity"
+                id="quantity2"
                 onChange={handleChange}
                 value={selectedProduct?.quantity}
+                required
               />
+              {/* <small>Error message</small> */}
               <label>Price: </label>
               <br />
               <input
@@ -420,8 +640,10 @@ function App() {
                 step="1"
                 className="form-control"
                 name="price"
+                id="price2"
                 onChange={handleChange}
                 value={selectedProduct?.price}
+                required
               />
             </div>
           </ModalBody>
@@ -439,6 +661,7 @@ function App() {
             {"   "}
           </ModalFooter>
         </Modal>
+        //  </form>
       }
 
       <Modal isOpen={modalDelete}>
